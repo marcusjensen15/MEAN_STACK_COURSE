102dcde8d3b7afbdf85dd25b0aa1c836f4b4a5f2
Marcus Jensen <marcusjensen1515@gmail.com>
2021-02-20 15:03:19 -0800

    post list now conditionally rendering if the post list length is greater than 0

--- src/app/post/post-list/post-list.component.ts
+++ src/app/post/post-list/post-list.component.ts
@@ -6,12 +6,12 @@ import { Component } from "@angular/core";
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent {
-  posts = [
-    {title: "First Post", content: "This is the first post"},
-    {title: "Second Post", content: "This is the second post"},
-    {title: "Third Post", content: "This is the third post"},
-    {title: "Fourth Post", content: "This is the fourth post"},
-
-  ];
+  // posts = [
+  //   {title: "First Post", content: "This is the first post"},
+  //   {title: "Second Post", content: "This is the second post"},
+  //   {title: "Third Post", content: "This is the third post"},
+  //   {title: "Fourth Post", content: "This is the fourth post"},
+  // ];
+  posts = [];
 
 }
